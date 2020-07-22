80001233b76de63e6e14e3474fd5db2d75a56834
Alexander Pilz <pilz@syslab.com>
2020-07-22 20:32:29 +0200

    Adding brand support

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,42 @@
 var js_sources = [
-          'js/jquery-2.2.4.min.js',
-          'js/jquery-ui.min.js',
-          'js/bootstrap.min.js',
-          'js/jquery-plugin-collection.js',
-          'js/fullcalendar-2.6.0/lang/de.js',
-          'js/revolution-slider/js/jquery.themepunch.tools.min.js',
-          'js/revolution-slider/js/jquery.themepunch.revolution.min.js',
-          'js/tsvensen-equalize.js-87c2cf1/js/equalize.js',
-          // 'js/underscore-min.js',
-          'js/revolution-slider/js/extensions/revolution.extension.actions.min.js',
-          'js/revolution-slider/js/extensions/revolution.extension.carousel.min.js',
-          'js/revolution-slider/js/extensions/revolution.extension.kenburn.min.js',
-          'js/revolution-slider/js/extensions/revolution.extension.layeranimation.min.js',
-          'js/revolution-slider/js/extensions/revolution.extension.migration.min.js',
-          'js/revolution-slider/js/extensions/revolution.extension.navigation.min.js',
-          'js/revolution-slider/js/extensions/revolution.extension.parallax.min.js',
-          'js/revolution-slider/js/extensions/revolution.extension.slideanims.min.js',
-          'js/revolution-slider/js/extensions/revolution.extension.video.min.js'
-        ];
+  'js/jquery-2.2.4.min.js',
+  'js/jquery-ui.min.js',
+  'js/bootstrap.min.js',
+  'js/jquery-plugin-collection.js',
+  'js/fullcalendar-2.6.0/lang/de.js',
+  'js/revolution-slider/js/jquery.themepunch.tools.min.js',
+  'js/revolution-slider/js/jquery.themepunch.revolution.min.js',
+  'js/tsvensen-equalize.js-87c2cf1/js/equalize.js',
+  // 'js/underscore-min.js',
+  'js/revolution-slider/js/extensions/revolution.extension.actions.min.js',
+  'js/revolution-slider/js/extensions/revolution.extension.carousel.min.js',
+  'js/revolution-slider/js/extensions/revolution.extension.kenburn.min.js',
+  'js/revolution-slider/js/extensions/revolution.extension.layeranimation.min.js',
+  'js/revolution-slider/js/extensions/revolution.extension.migration.min.js',
+  'js/revolution-slider/js/extensions/revolution.extension.navigation.min.js',
+  'js/revolution-slider/js/extensions/revolution.extension.parallax.min.js',
+  'js/revolution-slider/js/extensions/revolution.extension.slideanims.min.js',
+  'js/revolution-slider/js/extensions/revolution.extension.video.min.js'
+];
 var css_sources = [
-            'css/bootstrap.min.css', 
-            'css/jquery-ui.min.css',
-            'css/animate.css',
-            'css/css-plugin-collections.css',
-            'css/menuzord-skins/menuzord-rounded-boxed.css',
-            'css/style-main.css',
-            'css/custom-bootstrap-margin-padding.css',
-            'css/responsive.css',
-            'js/fullcalendar-3.9.0/fullcalendar.min.css',
-            'js/revolution-slider/css/settings.css',
-            'js/revolution-slider/css/layers.css',
-            'js/revolution-slider/css/navigation.css',
-            'css/colors/theme-skin-color-set-casc.css'
-          ];
-
-module.exports = function(grunt) {
+  'css/bootstrap.min.css',
+  'css/jquery-ui.min.css',
+  'css/animate.css',
+  'css/css-plugin-collections.css',
+  'css/menuzord-skins/menuzord-rounded-boxed.css',
+  'css/style-main.css',
+  'css/custom-bootstrap-margin-padding.css',
+  'css/responsive.css',
+  'js/fullcalendar-3.9.0/fullcalendar.min.css',
+  'js/revolution-slider/css/settings.css',
+  'js/revolution-slider/css/layers.css',
+  'js/revolution-slider/css/navigation.css',
+  'css/colors/theme-skin-color-set-casc.css'
+];
+var unibw_sources = css_sources.concat(['brands/unibw.css']);
+var dtec_sources = css_sources.concat(['brands/dtec.css']);
+console.log(dtec_sources);
+module.exports = function (grunt) {
 
   grunt.initConfig({
     concat: {
@@ -64,21 +66,11 @@ module.exports = function(grunt) {
       },
       target: {
         files: {
-          'css/unibw.min.css': css_sources
+          'css/unibw.min.css': unibw_sources,
+          'css/dtec.min.css': dtec_sources
         }
       }
-    },
-    // cssmin: {
-    //   target: {
-    //     files: [{
-    //       expand: true,
-    //       cwd: 'css/',
-    //       src: ['unibw.css'],
-    //       dest: 'css/',
-    //       ext: '.min.css'
-    //     }]
-    //   }
-    // }
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
