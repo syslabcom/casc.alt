c93307c24f4f5d5b8aa4cfc0bfdabc3505093964
Johannes Raggam <thetetet@gmail.com>
2021-10-11 15:32:41 +0200

    Compiled.

--- js/unibw.js
+++ js/unibw.js
@@ -19247,3 +19247,41 @@ var remVidfromList = function(_nc,opt) {
 if (window._old_require) {
     require = _old_require;
 }
+;var $ = jQuery;
+
+function live_equalizer(container_selector, item_selector) {
+    var news_listings = document.querySelectorAll(container_selector);
+    Array.prototype.forEach.call(news_listings, function (listing) {
+        var timeout = null;
+        var $listing = $(listing);
+        var observer;
+        var observer_options = {
+            subtree: true,
+            childList: true,
+            attributes: true,
+            attributeFilter: ["style"],
+        };
+
+        function equalizer() {
+            clearTimeout(timeout); // make sure, this isn't called too often.
+            timeout = setTimeout(function () {
+                observer.disconnect(); // need to disconnect because equalize is changing style attributes.
+                $listing.equalize({
+                    children: item_selector,
+                    equalize: "height",
+                    reset: true,
+                });
+                observer.observe(listing, observer_options);
+                console.debug("equalized");
+            }, 200);
+        }
+
+        observer = new MutationObserver(equalizer);
+        observer.observe(listing, observer_options);
+
+        $(window).resize(function () {
+            // restart on reload
+            equalizer();
+        });
+    });
+}
