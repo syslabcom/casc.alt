01b9bd23d20f3099d8d84d5a744867d27e219601
Johannes Raggam <thetetet@gmail.com>
2022-03-10 16:40:32 +0100

    wai: Improve equalizer function. Fixes: https://github.com/syslabcom/scrum/issues/130
    Basically we're not listening on style attribute changes anymore to prevent a but where we could not scroll to the very end.

--- js/unibw-custom.js
+++ js/unibw-custom.js
@@ -1,38 +1,41 @@
-var $ = jQuery;
+var live_equalizer;
+(function ($) {
+    var observer_options = {
+        subtree: true,
+        childList: true,
+        // Listening for style attribute changes results in not being able to scroll to the very end in some cases.
+        // See: https://github.com/syslabcom/scrum/issues/130
+        //attributes: true,
+        //attributeFilter: ["style"],
+    };
 
-function live_equalizer(container_selector, item_selector) {
-    var news_listings = document.querySelectorAll(container_selector);
-    Array.prototype.forEach.call(news_listings, function (listing) {
-        var timeout = null;
-        var $listing = $(listing);
-        var observer;
-        var observer_options = {
-            subtree: true,
-            childList: true,
-            attributes: true,
-            attributeFilter: ["style"],
-        };
+    live_equalizer = function (container_selector, item_selector) {
+        var listings = document.querySelectorAll(container_selector);
+        Array.prototype.forEach.call(listings, function (listing) {
+            var timeout = null;
+            var $listing = $(listing);
+            var observer;
 
-        function equalizer() {
-            clearTimeout(timeout); // make sure, this isn't called too often.
-            timeout = setTimeout(function () {
-                observer.disconnect(); // need to disconnect because equalize is changing style attributes.
-                $listing.equalize({
-                    children: item_selector,
-                    equalize: "height",
-                    reset: true,
-                });
-                observer.observe(listing, observer_options);
-                console.debug("equalized");
-            }, 200);
-        }
+            function equalizer() {
+                clearTimeout(timeout); // make sure, this isn't called too often.
+                timeout = setTimeout(function () {
+                    observer.disconnect(); // need to disconnect because equalize is changing style attributes.
+                    $listing.equalize({
+                        children: item_selector,
+                        equalize: "height",
+                        reset: true,
+                    });
+                    observer.observe($listing[0], observer_options);
+                    console.debug("equalized");
+                }, 200);
+            }
 
-        observer = new MutationObserver(equalizer);
-        observer.observe(listing, observer_options);
+            observer = new MutationObserver(equalizer);
+            observer.observe(listing, observer_options);
 
-        $(window).resize(function () {
-            // restart on reload
-            equalizer();
+            $(window).resize(function () {
+                equalizer();
+            });
         });
-    });
-}
+    };
+})(jQuery);
